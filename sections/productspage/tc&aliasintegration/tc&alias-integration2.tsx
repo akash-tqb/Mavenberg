dabe106dd84bb580965d7e6088f42e34cc47fa1c
akash-tqb <akashtqb06@gmail.com>
2023-08-10 16:17:51 +0530

    tc alias content added

--- sections/productspage/tc&aliasintegration/tc&alias-integration2.tsx
+++ sections/productspage/tc&aliasintegration/tc&alias-integration2.tsx
@@ -6,28 +6,20 @@ export default function TCAliasIntegration2() {
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
       <div className="lg:mb-20 mb-12">
         <h1 className="sm:text-5xl mb-6 text-3xl font-sans font-bold tracking-wide text-maven-white">
-          Unleash Your{" "}
-          <span className=" text-maven-blue">License Insights</span>
+        Work with a single{" "}
+          <span className=" text-maven-blue">source of truth</span>
         </h1>
         <h3 className="text-maven-white">
-          One-stop-shop to check the usage of various licenses through an
-          intuitive visual representation and therefore helps draw valuable
-          insight to better benefit the business and stretch the usage of those
-          expensive licenses to their maximum limit.
+        Incorporate Inventor files with your entire technical dataset, creating a unified repository of product information that fosters your end-to-end design-to-manufacturing workflow.
         </h3>
       </div>
       <div className="flex flex-col-reverse gap-8 lg:flex-row w-full p-8  lg:p-12 ">
         <div className="flex flex-col space-y-12 lg:w-1/2">
           <div className="flex flex-col gap-8">
-            <h1 className="text-maven-blue text-lg font-bold tracking-wide">
-              Featured Visualisation
+            <h1 className="text-maven-blue text-xl font-bold tracking-wide">
+              Featured
             </h1>
-            <p className="text-maven-white">
-              We have a list of specific visualisations that help you draw
-              insights on your PLM application license usage for customizable
-              inputs.
-            </p>
-            <ul className="flex flex-col gap-4">
+            <ul className="flex flex-col gap-4 text-lg">
               <li className="flex flex-row gap-4">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -43,7 +35,7 @@ export default function TCAliasIntegration2() {
                     d="M4.5 12.75l6 6 9-13.5"
                   />
                 </svg>
-                <p className="text-maven-white">Four Blocker graph</p>
+                <p className="text-maven-white">Automatically save the entire AutoDesk Alias Wireframe into Teamcenter.</p>
               </li>
               <li className="flex flex-row gap-4">
                 <svg
@@ -60,7 +52,7 @@ export default function TCAliasIntegration2() {
                     d="M4.5 12.75l6 6 9-13.5"
                   />
                 </svg>
-                <p className="text-maven-white">Trend graph</p>
+                <p className="text-maven-white">Open concept design from Teamcenter for viewing, modifying and cloning in AutoDesk Alias.</p>
               </li>
               <li className="flex flex-row gap-4">
                 <svg
@@ -77,7 +69,7 @@ export default function TCAliasIntegration2() {
                     d="M4.5 12.75l6 6 9-13.5"
                   />
                 </svg>
-                <p className="text-maven-white">License denied count graph</p>
+                <p className="text-maven-white">Simplify search, create and edit AutoDesk Alias objects, including drawings, parts and assemblies.</p>
               </li>
               <li className="flex flex-row gap-4">
                 <svg
@@ -95,13 +87,13 @@ export default function TCAliasIntegration2() {
                   />
                 </svg>
                 <p className="text-maven-white">
-                  Time/Application count based graph
+                Readily create and vault Direct Model files for easy digital mockup.
                 </p>
               </li>
             </ul>
           </div>
         </div>
-        <div className="lg:w-1/2  flex-col items-center hidden sm:mt-20 sm:mb-24 lg:my-0 sm:flex">
+        <div className="lg:w-1/2  flex-col items-center justify-center hidden sm:mt-20 sm:mb-24 lg:my-0 sm:flex">
           <div className="relative ">
             <div className="flex items-center justify-center rounded-full h-48 w-48 bg-gradient-radial from-maven-blue to-maven-bg">
               <div className="inline-flex items-center justify-center h-36 w-36 rounded-full bg-maven-bg border-2 border-maven-blue">
--- sections/productspage/tc&aliasintegration/tc&alias-integration3.tsx
+++ sections/productspage/tc&aliasintegration/tc&alias-integration3.tsx
@@ -4,146 +4,126 @@ export default function TCAliasIntegration3() {
   return (
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
       <div className="flex flex-col lg:mb-20 mb-12">
-        <h1 className="mb-16 font-sans text-3xl font-bold text-maven-white sm:text-5xl sm:leading-none">
+        <h1 className=" mb-12 font-sans text-3xl font-bold text-maven-white sm:text-5xl sm:leading-none">
           Benefits
         </h1>
         <div className="grid lg:grid-cols-3 lg:grid-rows-7 gap-8">
-          <div className="lg:row-span-4 flex flex-col gap-2 justify-end border p-4 rounded-lg border-maven-white">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-6 h-6 text-maven-blue"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4.5 12.75l6 6 9-13.5"
-                />
-              </svg>
+          <div className="lg:col-start-1 lg:row-end-5 lg:row-start-1 flex flex-col gap-2 justify-end border p-4 lg:p-8 rounded-lg border-maven-white">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-6 h-6 text-maven-blue"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4.5 12.75l6 6 9-13.5"
+              />
+            </svg>
             <h3 className="text-maven-blue text-xl font-bold ">
-                Benefits
-              </h3>
+              Centralized Data Management
+            </h3>
             <p className="text-maven-white/80 text-justify">
-              Manage procurement of licenses easily, Track accurate license
-              usage to a TEE, Make license planning and maintenance easy
+              All design-related information, including sketches, 3D models, and
+              associated documents, can be stored and managed in a central
+              repository.{" "}
             </p>
           </div>
-          <div className="lg:row-span-3 space-y-2 p-4 rounded-lg border border-maven-white">
-          <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-6 h-6 text-maven-orange"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4.5 12.75l6 6 9-13.5"
-                />
-              </svg>
-            <h3 className="text-maven-orange text-xl font-bold">
-                Benefits
-              </h3>
-            <p className="text-maven-white/80 text-justify">
-              Manage procurement of licenses easily, Track accurate license
-              usage to a TEE
-            </p>
-          </div>
-          <div className="lg:row-span-4 gap-2 flex flex-col justify-end border p-4 rounded-lg border-maven-white">
-          <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-6 h-6 text-maven-blue"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4.5 12.75l6 6 9-13.5"
-                />
-              </svg>
+          <div className=" lg:row-end-[8] lg:row-start-1 lg:col-start-2 gap-2 flex flex-col justify-center  border p-4 lg:p-8 rounded-lg border-maven-white">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-6 h-6 text-maven-blue"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4.5 12.75l6 6 9-13.5"
+              />
+            </svg>
             <h3 className="text-maven-blue text-xl font-bold">
-                Benefits
-              </h3>
+              Version Control
+            </h3>
             <p className="text-maven-white/80 text-justify">
-              Manage procurement of licenses easily, Track accurate license
-              usage to a TEE, Make license planning and maintenance easy
+              Manage different design iterations and versions within Alias. This
+              ensures that the most up-to-date design is readily available and
+              reduces confusion caused by outdated or conflicting versions.{" "}
             </p>
           </div>
-          <div className="lg:col-start-1 lg:row-span-3 p-4 space-y-2 rounded-lg border border-maven-white">
-          <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-6 h-6 text-maven-orange"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4.5 12.75l6 6 9-13.5"
-                />
-              </svg>
-            <h3 className="text-maven-orange text-xl font-bold">
-                Benefits
-              </h3>
-            <p className="text-maven-white/80 text-justify">
-              Manage procurement of licenses easily, Track accurate license
-              usage to a TEE
-            </p>
-          </div>
-          <div className="lg:row-span-4 gap-2 flex flex-col justify-end lg:col-start-2 lg:row-start-4 border p-4 rounded-lg border-maven-white">
-          <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-6 h-6 text-maven-blue"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4.5 12.75l6 6 9-13.5"
-                />
-              </svg>
+          <div className="lg:row-span-4 lg:col-start-3 gap-2 flex flex-col justify-end border p-4 lg:p-8 rounded-lg border-maven-white">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-6 h-6 text-maven-blue"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4.5 12.75l6 6 9-13.5"
+              />
+            </svg>
             <h3 className="text-maven-blue text-xl font-bold">
-                Benefits
-              </h3>
+              Time Efficient
+            </h3>
             <p className="text-maven-white/80 text-justify">
-              Manage procurement of licenses easily, Track accurate license
-              usage to a TEE, Make license planning and maintenance easy
+              Maximize collaboration and save time finding the correct revisions
+              of Alias data.
             </p>
           </div>
-          <div className="lg:row-span-3 space-y-2 p-4 rounded-lg border border-maven-white">
-          <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                className="w-6 h-6 text-maven-orange"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4.5 12.75l6 6 9-13.5"
-                />
-              </svg>
+          <div className="lg:col-start-1 lg:row-span-3 p-4 lg:p-8 space-y-2 rounded-lg border border-maven-white">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-6 h-6 text-maven-orange"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4.5 12.75l6 6 9-13.5"
+              />
+            </svg>
             <h3 className="text-maven-orange text-xl font-bold">
-                Benefits
-              </h3>
+              Simplified Workflows
+            </h3>
             <p className="text-maven-white/80 text-justify">
-              Manage procurement of licenses easily, Track accurate license
-              usage to a TEE
+              Create automated workflows that trigger certain actions based on
+              design milestones or approvals. This simplifies and accelerates
+              design processes.{" "}
+            </p>
+          </div>
+
+          <div className="lg:row-span-3 lg:col-start-3 space-y-2 p-4 lg:p-8 rounded-lg border border-maven-white">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-6 h-6 text-maven-orange"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4.5 12.75l6 6 9-13.5"
+              />
+            </svg>
+            <h3 className="text-maven-orange text-xl font-bold">Scalability</h3>
+            <p className="text-maven-white/80 text-justify">
+              As your design projects and team grow, scalability is ensured by
+              providing a structured environment for managing and expanding your
+              design data and processes.{" "}
             </p>
           </div>
         </div>
