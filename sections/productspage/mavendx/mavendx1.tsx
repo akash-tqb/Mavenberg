2101dd867853e6f9bd8266e53f6ddcf004174fae
akash-tqb <akashtqb06@gmail.com>
2023-08-14 13:05:14 +0530

    data in second line

--- sections/productspage/mavendx/mavendx1.tsx
+++ sections/productspage/mavendx/mavendx1.tsx
@@ -3,7 +3,7 @@ export default function MavenDx1(){
         <section className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
         <div className="space-y-5 max-w-4xl mx-auto text-center">
           <h2 className="text-4xl lg:mb-12 text-maven-white font-extrabold mx-auto md:text-5xl">
-          Propelling Your Team Towards {" "}
+          Propelling Your Team Towards{" "}<br/>
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-maven-blue to-maven-orange">
             Data-Driven Growth and Innovation 
             </span>
