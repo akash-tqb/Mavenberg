575c8511915315e78dc7e7eac0081a0e9349cab5
akash-tqb <akashtqb06@gmail.com>
2023-08-15 10:50:41 +0530

    v0.19

--- sections/servicespage/plmmanaged-services1.tsx
+++ sections/servicespage/plmmanaged-services1.tsx
@@ -5,11 +5,11 @@ export default function PlmManagedServices1() {
         <h1 className="text-4xl text-maven-white font-extrabold mx-auto md:text-5xl mb-12">
           PLM{" "}
           <span className="text-transparent bg-clip-text bg-gradient-to-r from-maven-blue to-maven-orange">
-          Managed Services
+            Managed Services
           </span>
         </h1>
         <h1 className="mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-maven-blue sm:text-4xl md:mx-auto">
-          Cloud{' '}<span className="text-maven-white">Infrastructure</span>
+          Cloud <span className="text-maven-white">Infrastructure</span>
         </h1>
         <p className="text-maven-white">
           Our cloud infrastructure management solutions facilitate the
@@ -19,63 +19,83 @@ export default function PlmManagedServices1() {
         </p>
       </div>
       <div className="grid gap-5 mb-8 md:grid-cols-2 lg:grid-cols-3">
-      <div className="duration-300 transform border-maven-blue border border-l-4  shadow-sm hover:-translate-y-2">
-          <div className="h-full p-5 border border-l-0 border-maven-blue rounded-r shadow-sm">
-          <h6 className="mb-2 font-semibold leading-5 text-maven-blue">
-            Resource Provisioning
-          </h6>
-          <p className=" text-maven-white">
-          We strategically allocate and optimize computing resources for uninterrupted application performance.  We ensure optimal operation during peak usage, enhancing cost-efficiency and aligning your IT infrastructure seamlessly with evolving business demands
-          </p>
-          </div>
-        </div>
         <div className="duration-300 transform border-maven-blue border border-l-4  shadow-sm hover:-translate-y-2">
           <div className="h-full p-5 border border-l-0 border-maven-blue rounded-r shadow-sm">
-          <h6 className="mb-2 font-semibold leading-5 text-maven-blue">
-          24/7 Hardware Support
-          </h6>
-          <p className=" text-maven-white">
-          Our 24/7 Hardware Support offers continuous technical assistance for your physical hardware infrastructure. Our skilled technicians are available around the clock to address hardware issues promptly
-          </p>
-          </div>
-        </div>
-        <div className="duration-300 transform border-maven-blue border border-l-4  shadow-sm hover:-translate-y-2">
-          <div className="h-full p-5 border border-l-0 border-maven-blue rounded-r shadow-sm">
-          <h6 className="mb-2 font-semibold leading-5 text-maven-blue">
-          8/5 L2 Support
-          </h6>
-          <p className=" text-maven-white">
-          Our skilled Level 2 engineers offer comprehensive technical assistance during business hours. This goes beyond basic troubleshooting and covers complex tasks like software configurations, application integrations, and performance optimization. We collaborate closely with your team to promptly enhance IT stability and reliability
-          </p>
-          </div>
-        </div>
-        <div className="duration-300 transform border-maven-blue border border-l-4  shadow-sm hover:-translate-y-2">
-          <div className="h-full p-5 border border-l-0 border-maven-blue rounded-r shadow-sm">
-          <h6 className="mb-2 font-semibold leading-5 text-maven-blue">
-            Cost Optimization
-          </h6>
-          <p className=" text-maven-white">
-            Analyze usage patterns and optimize resource allocation to minimize
-            costs
-          </p>
+            <h6 className="mb-2 font-semibold leading-5 text-maven-blue">
+              Resource Provisioning
+            </h6>
+            <p className=" text-maven-white">
+              We strategically allocate and optimize computing resources for
+              uninterrupted application performance. We ensure optimal operation
+              during peak usage, enhancing cost-efficiency and aligning your IT
+              infrastructure seamlessly with evolving business demands
+            </p>
           </div>
         </div>
         <div className="duration-300 transform border-maven-blue border border-l-4  shadow-sm hover:-translate-y-2">
           <div className="h-full p-5 border border-l-0 border-maven-blue rounded-r shadow-sm">
             <h6 className="mb-2 font-semibold leading-5 text-maven-blue">
-            License Tracking
+              24/7 Hardware Support
             </h6>
             <p className=" text-maven-white">
-            We guarantee compliant and cost-effective software usage. Our meticulous license management includes tracking usage, renewals, and updates, helping you avoid financial risks. By optimizing licenses, we guide informed software procurement and cost savings            </p>
+              Our 24/7 Hardware Support offers continuous technical assistance
+              for your physical hardware infrastructure. Our skilled technicians
+              are available around the clock to address hardware issues promptly
+            </p>
           </div>
         </div>
         <div className="duration-300 transform border-maven-blue border border-l-4  shadow-sm hover:-translate-y-2">
           <div className="h-full p-5 border border-l-0 border-maven-blue rounded-r shadow-sm">
             <h6 className="mb-2 font-semibold leading-5 text-maven-blue">
-            Application Incident Support
+              8/5 L2 Support
             </h6>
             <p className=" text-maven-white">
-            We ensure your software applications' health and functionality. Our specialized team promptly diagnoses, analyzes, and resolves incidents. Collaborating with your developers, we identify root causes to swiftly restore normal operation and minimize disruptions
+              Our capable Level 2 engineers who deliver comprehensive technical
+              aid during business hours. In addition to troubleshooting, we
+              handle intricate tasks like software configurations, application
+              integrations, and performance optimization to enhance IT stability
+              and reliability
+            </p>
+          </div>
+        </div>
+        <div className="duration-300 transform border-maven-blue border border-l-4  shadow-sm hover:-translate-y-2">
+          <div className="h-full p-5 border border-l-0 border-maven-blue rounded-r shadow-sm">
+            <h6 className="mb-2 font-semibold leading-5 text-maven-blue">
+              Cost Optimization
+            </h6>
+            <p className=" text-maven-white">
+              Our service specializes in enhancing cost efficiency within your
+              cloud infrastructure. By meticulously analyzing and strategically
+              adjusting resource allocation, we help you maximize value,
+              minimize expenses, and allocate your cloud budget effectively for
+              sustainable growth
+            </p>
+          </div>
+        </div>
+        <div className="duration-300 transform border-maven-blue border border-l-4  shadow-sm hover:-translate-y-2">
+          <div className="h-full p-5 border border-l-0 border-maven-blue rounded-r shadow-sm">
+            <h6 className="mb-2 font-semibold leading-5 text-maven-blue">
+              License Tracking
+            </h6>
+            <p className=" text-maven-white">
+              We guarantee compliant and cost-effective software usage. Our
+              meticulous license management includes tracking usage, renewals,
+              and updates, helping you avoid financial risks. By optimizing
+              licenses, we guide informed software procurement and cost savings{" "}
+            </p>
+          </div>
+        </div>
+        <div className="duration-300 transform border-maven-blue border border-l-4  shadow-sm hover:-translate-y-2">
+          <div className="h-full p-5 border border-l-0 border-maven-blue rounded-r shadow-sm">
+            <h6 className="mb-2 font-semibold leading-5 text-maven-blue">
+              Application Incident Support
+            </h6>
+            <p className=" text-maven-white">
+              We ensure your software applications' health and functionality.
+              Our specialized team promptly diagnoses, analyzes, and resolves
+              incidents. Collaborating with your developers, we identify root
+              causes to swiftly restore normal operation and minimize
+              disruptions
             </p>
           </div>
         </div>
