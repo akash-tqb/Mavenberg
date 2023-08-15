7f498a1f7b477c269789bc0693b4b68f3af25f3c
akash-tqb <akashtqb06@gmail.com>
2023-08-15 07:12:38 +0530

    Version 0.11

--- sections/productspage/licenseInsights/license-insights1.tsx
+++ sections/productspage/licenseInsights/license-insights1.tsx
@@ -14,20 +14,24 @@ export default function ProductsPageSection1() {
         <div className="flex items-center justify-center -mx-4 lg:pl-8 lg:w-1/2">
         <div className="flex flex-col items-end px-3">
             <Image
-            width={300}
-            height={300}
+            width={500}
+            height={500}
               className="object-cover mb-6 rounded shadow-lg h-28 sm:h-48 xl:h-56 w-28 sm:w-48 xl:w-56"
               src="/licenserealtime3.png"
               alt=""
             />
-            <img
+            <Image
+            width={500}
+            height={500}
               className="object-cover w-20 h-20 rounded shadow-lg sm:h-32 xl:h-40 sm:w-32 xl:w-40"
               src="/licenserealtime2.png"
               alt=""
             />
           </div>
           <div className="px-3">
-            <img
+            <Image
+            width={500}
+            height={500}
               className="object-cover w-40 h-40 rounded shadow-lg sm:h-64 xl:h-80 sm:w-64 xl:w-80"
               src="/licenserealtime1.png"
               alt=""
