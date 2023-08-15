b54fa10fba09d60a45e54fab96e9ca5773e66667
akash-tqb <akashtqb06@gmail.com>
2023-08-15 09:46:37 +0530

    v0.17

--- sections/servicespage/simulation-services1.tsx
+++ sections/servicespage/simulation-services1.tsx
@@ -1,6 +1,6 @@
 export default function SimulationServices1() {
   return (
-    <section className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+    <section className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:pt-20">
       <div className="space-y-5 max-w-4xl mx-auto text-center mb-12">
         <h1 className="text-4xl text-transparent bg-clip-text bg-gradient-to-r from-maven-blue to-maven-orange font-extrabold mx-auto md:text-5xl mb-12">
           Simulation{" "}
