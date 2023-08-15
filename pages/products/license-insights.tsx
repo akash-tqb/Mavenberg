5ed3a1c30453edcbd475d120e8553a9344eaa08d
akash-tqb <akashtqb06@gmail.com>
2023-08-15 08:54:33 +0530

    v0.14

--- pages/products/license-insights.tsx
+++ pages/products/license-insights.tsx
@@ -1,13 +1,11 @@
 import ProductsPageSection1 from "@/sections/productspage/licenseInsights/license-insights1";
 import ProductsPageSection2 from "@/sections/productspage/licenseInsights/license-insights2";
-import ProductsPageSection3 from "@/sections/productspage/licenseInsights/license-insight3";
 
 export default function ProductsPage() {
   return (
     <>
       <ProductsPageSection1 />
       <ProductsPageSection2 />
-      {/* <ProductsPageSection3 /> */}
     </>
   );
 }
